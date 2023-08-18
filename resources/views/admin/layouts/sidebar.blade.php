8ec8ccd46e155f144ec0616b4e700960c1dbfeea
vivek <ghongatevivek1212@gmail.com>
2023-08-18 22:07:56 +0530

    added changes in sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -9,7 +9,18 @@
       <span>Dashboard</span>
     </a>
   </li><!-- End Dashboard Nav -->
-
+  <li class="nav-item">
+    <a class="nav-link " href="index.html">
+      <i class="bi bi-users"></i>
+      <span>Manage Users</span>
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link " href="{{route('remindertype.index')}}">
+      <i class="bi bi-users"></i>
+      <span>Manage Remindar Types</span>
+    </a>
+  </li>
 
 </ul>
 
