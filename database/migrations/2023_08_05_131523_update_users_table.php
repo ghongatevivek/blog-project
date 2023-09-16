53b110f256e9bcc4d808ffde3b399078589f812a
ghongatevivek <ghongatevivek1212@gmail.com>
2023-09-16 17:44:26 +0530

    Added nullable in mobile no

--- database/migrations/2023_08_05_131523_update_users_table.php
+++ database/migrations/2023_08_05_131523_update_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('mobile',20);
+            $table->string('mobile',20)->nullable();
         });
     }
 
